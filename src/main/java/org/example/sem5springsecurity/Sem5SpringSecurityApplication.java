da714f89a49942856ff29e7fced04db164f19f76
mateonoel2 <mateo.noel@utec.edu.pe>
2024-04-30 15:52:44 -0500

    paso 1

---
+++ src/main/java/org/example/sem5springsecurity/Sem5SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.sem5springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem5SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem5SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sem5springsecurity/Sem5SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sem5springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem5SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
